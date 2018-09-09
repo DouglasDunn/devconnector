6068ce77effbc97912c6d6e8ccaf96ee94db6a18
Douglas Dunn <douglas.dunn@outlook.com>
2018-09-09 09:04:18 -0700

    Posts and Comments

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -6,7 +6,9 @@ export default function(state = initialState, action) {
   switch (action.type) {
     case GET_ERRORS:
       return action.payload;
-    default:
-      return state;
+      case CLEAR_ERRORS:
+        return {};
+      default:
+        return state;
   }
 }
